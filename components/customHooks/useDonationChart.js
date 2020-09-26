71c2796d4f5589baec4f05a297369a7642377b04
Elliot Zoerner <eliot.zoerner@gmail.com>
2020-09-26 23:04:40 +0930

    Reduce indentation by returning early

--- components/customHooks/useDonationChart.js
+++ components/customHooks/useDonationChart.js
@@ -59,106 +59,107 @@ export const useDonationChart = (props) => {
   useEffect(() => {
     const parsedColors = colors.slice();
     const colorsToRemove = [];
+    
+    if (!donationHistory || donationHistory.length === 0) {
+      return;
+    }
 
-    if (donationHistory && donationHistory.length !== 0) {
-      const parsedData = [["Date"]];
+    const parsedData = [["Date"]];
+    
+    // Add the names of the people
+    const people = donorAmounts.filter((person, i) => {
+      // exclude excluded players
+      const isExcluded = excludedPeople.includes(person[0]);
+      if (isExcluded) {
+        colorsToRemove.push(i);
+      }
 
-      // Add the names of the people
-      const people = donorAmounts.filter((person, i) => {
-        // exclude excluded players
-        const isExcluded = excludedPeople.includes(person[0]);
-        if (isExcluded) {
-          colorsToRemove.push(i);
-        }
+      return !isExcluded;
+    }).map(person => person[0]);
+    parsedData[0].push(...people);
+    
+    // Get the interval for the chart
+    const currentTime = new Date();
+    const { interval, samplesInDomain } = chartDomains[chartDomainIndex];
+    const startTimestamp = currentTime - interval * samplesInDomain;
+    
+    // Add a row for each interval
+    let scrapeIterator = 0;
+    let scrape;
+    let prevScrape;
+    for (
+      let timestamp = startTimestamp;
+      timestamp <= currentTime;
+      timestamp += interval
+    ) {
+      const xLabel = distanceInWordsStrict(new Date(timestamp), currentTime) + " ago";
 
-        return !isExcluded;
-      }).map(person => person[0]);
-      parsedData[0].push(...people);
-
-      // Get the interval for the chart
-      const currentTime = new Date();
-      const { interval, samplesInDomain } = chartDomains[chartDomainIndex];
-      const startTimestamp = currentTime - interval * samplesInDomain;
-
-      // Add a row for each interval
-      let scrapeIterator = 0;
-      let scrape;
-      let prevScrape;
-      for (
-        let timestamp = startTimestamp;
-        timestamp <= currentTime;
-        timestamp += interval
+      // Find a scrape for this timestamp
+      scrape = donationHistory[scrapeIterator];
+      while (
+        scrape.date < timestamp &&
+        scrapeIterator < donationHistory.length - 1
       ) {
-        const xLabel =
-          distanceInWordsStrict(new Date(timestamp), currentTime) + " ago";
-
-        // Find a scrape for this timestamp
+        scrapeIterator += 1;
+        prevScrape = scrape;
         scrape = donationHistory[scrapeIterator];
-        while (
-          scrape.date < timestamp &&
-          scrapeIterator < donationHistory.length - 1
-        ) {
-          scrapeIterator += 1;
-          prevScrape = scrape;
-          scrape = donationHistory[scrapeIterator];
-        }
+      }
 
-        const isScrapeTooRecent = scrape.date > timestamp + interval;
-        if (isScrapeTooRecent) {
-          if (parsedData.length === 1) {
-            // No data added yet
-            if (!prevScrape) {
-              // Records don't go back this far, so push an undefined row
-              parsedData.push([xLabel, ...people.map(el => undefined)]);
-            } else {
-              // Use the previous scrape as a starting point
-              const amounts = people.map((name, i) => {
-                const person = prevScrape.donationData.find(el => el.name === name);
-                if (person) return person.amount;
-                const lastValue = parsedData[parsedData.length - 1][i + 1];
-                return typeof lastValue === "string" ? undefined : lastValue;
-              });
-
-              parsedData.push([xLabel, ...amounts]);
-            }
+      const isScrapeTooRecent = scrape.date > timestamp + interval;
+      if (isScrapeTooRecent) {
+        if (parsedData.length === 1) {
+          // No data added yet
+          if (!prevScrape) {
+            // Records don't go back this far, so push an undefined row
+            parsedData.push([xLabel, ...people.map(el => undefined)]);
           } else {
-            // Append the data from the previous interval
-            const prevRow = parsedData[parsedData.length - 1];
-            parsedData.push([xLabel, ...prevRow.slice(1)]);
+            // Use the previous scrape as a starting point
+            const amounts = people.map((name, i) => {
+              const person = prevScrape.donationData.find(el => el.name === name);
+              if (person) return person.amount;
+              const lastValue = parsedData[parsedData.length - 1][i + 1];
+              return typeof lastValue === "string" ? undefined : lastValue;
+            });
+
+            parsedData.push([xLabel, ...amounts]);
           }
         } else {
-          // Get each person's amount from scrape
-          const amounts = people.map((name, i) => {
-            const person = scrape.donationData.find(el => el.name === name);
-            if (person) return person.amount;
-
-            // Person not found in scrape
-            const lastValue = parsedData[parsedData.length - 1][i + 1];
-            if (timestamp + interval > currentTime) {
-              // this is the last iteration
-              if (typeof lastValue === "string" || lastValue === undefined) {
-                const currentAmount = donorAmounts.find(
-                  el => el[0] === name
-                )[1];
-                return currentAmount;
-              }
-            }
-            return typeof lastValue === "string" ? undefined : lastValue;
-          });
-
-          parsedData.push([xLabel, ...amounts]);
+          // Append the data from the previous interval
+          const prevRow = parsedData[parsedData.length - 1];
+          parsedData.push([xLabel, ...prevRow.slice(1)]);
         }
-      }
+      } else {
+        // Get each person's amount from scrape
+        const amounts = people.map((name, i) => {
+          const person = scrape.donationData.find(el => el.name === name);
+          if (person) return person.amount;
 
-      for (let i = colorsToRemove.length - 1; i >= 0; i--) {
-        parsedColors.splice(colorsToRemove[i], 1);
+          // Person not found in scrape
+          const lastValue = parsedData[parsedData.length - 1][i + 1];
+          if (timestamp + interval > currentTime) {
+            // this is the last iteration
+            if (typeof lastValue === "string" || lastValue === undefined) {
+              const currentAmount = donorAmounts.find(
+                el => el[0] === name
+              )[1];
+              return currentAmount;
+            }
+          }
+          return typeof lastValue === "string" ? undefined : lastValue;
+        });
+
+        parsedData.push([xLabel, ...amounts]);
       }
-      setParsedDonations(parsedData);
-      setChartOptions({
-        ...chartOptions,
-        colors: parsedColors
-      });
     }
+
+    for (let i = colorsToRemove.length - 1; i >= 0; i--) {
+      parsedColors.splice(colorsToRemove[i], 1);
+    }
+    setParsedDonations(parsedData);
+    setChartOptions({
+      ...chartOptions,
+      colors: parsedColors
+    });
   }, [chartDomainIndex, donationHistory, donorAmounts, excludedPeople]);
 
   const amount = donorAmounts.reduce((acc, donor) => acc + donor[1], 0);
