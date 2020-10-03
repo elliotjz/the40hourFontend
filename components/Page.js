6a1cad1c6c77d3b2ed039c890001b00043160da8
Elliot Zoerner <eliot.zoerner@gmail.com>
2020-10-03 14:32:12 +0930

    Extract Footer component

--- components/Page.js
+++ components/Page.js
@@ -1,5 +1,6 @@
 import { usePage } from "../hooks/usePage";
 import Header from "./Header";
+import Footer from "./Footer";
 import DonationChart from "./DonationChart";
 import { useDonationData } from "../contexts/DonationDataContext";
 
@@ -43,24 +44,7 @@ const Page = () => {
       <button onClick={scrapeDonationPages} disabled={loading}>
         Scrape
       </button>
-      <footer className="footer">
-        <p>
-          <a href="https://www.the40hourjammin.com">The 40 hour jammin</a> is
-          being held from May 17 until 19 on Magnetic Island, Queensland. To
-          support the cause and move your favorite musician up the leaderboard,{" "}
-          <a href="https://www.the40hourjammin.com/artists">donate</a>.
-        </p>
-        <p>
-          The leaderboard gets the donation amounts from the facebook donation
-          pages every 15 minutes, and will update more frequently once the 40
-          hour jammin starts.
-        </p>
-        <p>
-          This project is open source and was made by Elliot Zoerner. Find the
-          source code{" "}
-          <a href="https://github.com/elliotjz/the40hourFontend">here</a>.
-        </p>
-      </footer>
+      <Footer />
     </div>
   );
 };
