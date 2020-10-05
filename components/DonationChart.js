49a685f0c25d6a392932bb1daef137e980267087
Elliot Zoerner <eliot.zoerner@gmail.com>
2020-10-05 16:00:51 +1030

    Return null early if parseDonations is not ready

--- components/DonationChart.js
+++ components/DonationChart.js
@@ -9,26 +9,26 @@ import LoadingIndicator from "./LoadingIndicator";
 const DonationChart = (props) => {
   const { chartOptions, parsedDonations } = props;
 
+  if (!parsedDonations || !parsedDonations[0]) {
+    return null;
+  }
+
   return (
     <div className="donation-chart">
       <div className="chips-wrapper">
         <Chips colors={colors} />
-        {parsedDonations && parsedDonations[0].length > 1 && (
-          <div>
-            <Chart
-              chartType="LineChart"
-              width="100%"
-              height="var(--chart-height)"
-              data={parsedDonations}
-              options={chartOptions}
-              loader={
-                <div className="chart-spinner-container">
-                  <LoadingIndicator />
-                </div>
-              }
-            />
-          </div>
-        )}
+        <Chart
+          chartType="LineChart"
+          width="100%"
+          height="var(--chart-height)"
+          data={parsedDonations}
+          options={chartOptions}
+          loader={
+            <div className="chart-spinner-container">
+              <LoadingIndicator />
+            </div>
+          }
+        />
       </div>
     </div>
   );
